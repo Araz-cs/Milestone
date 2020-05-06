51b217853f8ae6e068a6b89929375498c4d04753
Araz-cs <52028482+Araz-cs@users.noreply.github.com>
2020-05-05 23:13:01 -0700

    relative path added

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 #path = Path('C:/Users/Araz/Desktop/121/Asg#3/DEV')
-path = os.path.dirname(os.path.realpath(__file__))
+path =path = os.path.dirname(os.path.realpath(__file__))  + '/DEV'
 for subdir, dirs, files in os.walk(path):
     for filename in files:
         filepath = subdir + os.sep + filename
