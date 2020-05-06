d027ddc4b016995630e0be2f668e447abc693084
Araz-cs <52028482+Araz-cs@users.noreply.github.com>
2020-05-05 23:08:16 -0700

    relative path added

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import json
 import os
 from pathlib import Path
 
-path = Path('C:/Users/Araz/Desktop/121/Asg#3/DEV')
+#path = Path('C:/Users/Araz/Desktop/121/Asg#3/DEV')
+path = os.path.dirname(os.path.realpath(__file__))
 for subdir, dirs, files in os.walk(path):
     for filename in files:
         filepath = subdir + os.sep + filename
