4ded43291bd5fcd1ed33f723267b29fdfde2eb3e
Pierce Taylor <iampiercetaylor@gmail.com>
2020-05-06 23:14:21 -0700

    parsed the content from the json files

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
 import json
 import os
+import re
 from pathlib import Path
+from bs4 import BeautifulSoup, Comment
 
-path = Path('C:/Users/Araz/Desktop/121/Asg#3/DEV')
+ary = []
+path = os.path.dirname(os.path.realpath(__file__))  + '\DEV'
 for subdir, dirs, files in os.walk(path):
     for filename in files:
         filepath = subdir + os.sep + filename
-
         if filepath.endswith(".json"):
-            print (filepath)
+            #print (filepath)
+            files = open(filepath)
+            for i in files:
+                json_load = json.loads(i)
+           
+            soup = BeautifulSoup(json_load['content'], "lxml")
+
+            for tag in soup(text=lambda text: isinstance(text,Comment)):
+                tag.extract()
+
+            for element in soup.findAll(['script', 'style']):
+                element.extract()
+
+            space_delemited_text = re.sub('\s+',' ',soup.get_text())
+            print (space_delemited_text)
+            x = input()
